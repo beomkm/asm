82f5ac38e73a82e659365405da001c27651d5e87
Beom-yeong Kim <tibyte@naver.com>
2016-08-20 01:00:33 +0900

    Create unary.c

---
+++ unary.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	a = 5;
+	b = a--;
+	printf("b = a--;\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = a++;
+	printf("b = a++;\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = ++a;
+	printf("b = ++a;\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = a + (a++);
+	printf("b = a + (a++);\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = (a++) + a;
+	printf("b = (a++) + a;\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = a + (++a);
+	printf("b = a + (++a);\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = (++a) + a;
+	printf("b = (++a) + a;\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = (a++) + (a++);
+	printf("b = (a++) + (a++);\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = (++a) + (++a);
+	printf("b = (++a) + (++a);\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = (a++) + (++a);
+	printf("b = (a++) + (++a);\n");
+	printf("b=%d\n\n", b);
+
+	a = 5;
+	b = (++a) + (a++);
+	printf("b = (++a) + (a++);\n");
+	printf("b=%d\n\n", b);
+
+
+	return 0;
+}
